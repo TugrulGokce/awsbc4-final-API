3e22433af79750dd800517111693a52c80f7af3b
Tugrul <tudi2882@gmail.com>
2023-03-24 17:51:20 +0300

    dynamodb connection added

--- gateio_websocket.py
+++ gateio_websocket.py
@@ -1,10 +1,18 @@
 import asyncio
 import websockets
 import json
-
+import boto3
+from loguru import logger
 
 # TODO bazı websocketlerde time olarak unixtimestamp var bazılarında düz tarih var hangi formatta db'ye kaydolacak
 # TODO gate.io icin unixtimestamp
+# TODO dynamodb de partition key ney'e göre olacak.
+
+dynamo_db_client = boto3.resource('dynamodb')
+table = dynamo_db_client.Table('coinTrade')
+logger.success("Succesfully connected to dynamodb")
+
+
 async def subscribe_gate_io():
     url = "wss://api.gateio.ws/ws/v4/"
     async with websockets.connect(url) as websocket:
@@ -16,12 +24,21 @@ async def subscribe_gate_io():
         await websocket.send(json.dumps(subscribe_request))
         while True:
             response = await websocket.recv()
-            print(response)
             data_list = json.loads(response)
-            print(data_list)
+            # print(data_list)
             if data_list['event'] == 'update':
-                print(f"trade_pairs : {data_list['result']['currency_pair']} | price : {data_list['result']['price']} "
-                      f"| time : {data_list['time']}")
+                table.put_item(
+                    Item={
+                        'trade_id': data_list['result']['id'],
+                        'trade_pair_name': data_list['result']['currency_pair'],
+                        'price': data_list['result']['price'],
+                        'unix_timestamp': data_list['time'],
+                    },
+                )
+                logger.info(f"Item --> trade_id: {data_list['result']['id']} | "
+                            f"trade_pair_name : {data_list['result']['currency_pair']} | "
+                            f"price : {data_list['result']['price']} | "
+                            f"time : {data_list['time']} succesfully added to 'coinTrade' table")
 
 
 if __name__ == '__main__':
