0c3c123d10e793206b1cc44cdb7bf0f004054bc0
MSAHIN <90087613+datasciengine@users.noreply.github.com>
2023-03-30 22:27:30 +0300

    Update Table name

--- db_write.py
+++ db_write.py
@@ -4,7 +4,7 @@ import boto3
 class DBWriter:
     def __init__(self):
         dynamodb = boto3.resource('dynamodb')
-        self.table = dynamodb.Table('Arbitrage')
+        self.table = dynamodb.Table('arbitrage_btc')
 
     def write(self, message):
         self.table.put_item(Item={'id': message[0],
