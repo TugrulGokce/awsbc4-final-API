b547f1e33bc62c5ecd91f25a89d1372556067d81
MSAHIN <90087613+datasciengine@users.noreply.github.com>
2023-04-04 14:22:30 +0300

    Add paramter features.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from writer_dynamodb_s3_website.write_dynamo import run_write_dynamodb
 from reader_dynamodb_notification.read_dynamo import run_read_dynamo_sns
 import threading
+import sys
 
-sec, diff_usd = 60, 50
+sec, diff_usd = int(sys.argv[1]), int(sys.argv[2])
+print("Second ", sec)
+print("Diff USD", diff_usd)
 
 t_write = threading.Thread(target=run_write_dynamodb)
 t_read = threading.Thread(target=run_read_dynamo_sns, args=(sec, diff_usd))
