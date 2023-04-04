c55664d34d60b19bb3773124458cfe1d00c39844
MSAHIN <90087613+datasciengine@users.noreply.github.com>
2023-04-04 13:39:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from writer_dynamodb_s3_website.write_dynamo import run_write_dynamodb
 from reader_dynamodb_notification.read_dynamo import run_read_dynamo_sns
 import threading
 
+sec, diff_usd = 60, 50
+
 t_write = threading.Thread(target=run_write_dynamodb)
 t_read = threading.Thread(target=run_read_dynamo_sns, args=(sec, diff_usd))
 
